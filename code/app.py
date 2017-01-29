49939e04bf352779426d8cb402c229d1fa75ed82
Robert Eisele <robert@xarg.org>
2017-01-29 23:55:53 +0100

    added emoworker

--- code/app.py
+++ code/app.py
@@ -3,9 +3,94 @@ import tornado as tornado
 import tornado.web as web
 import tornado.websocket as websocket
 import cv2
-import os
+import os, time
 import painter.submit_painting as sp
 from sys import platform
+from emokit.emotiv import Emotiv
+import threading
+import numpy as np
+
+
+WINDOW_SIZE = 250
+SD_UPPER_LIMIT = 100
+SD_LOWER_LIMIT = 50
+
+class EmoWorker:
+
+    def __init__(self):
+        threading.Thread(target=self.do_start, name="_gen_").start()
+        self.run = True
+        self.last_values_list = [0 for i in range(WINDOW_SIZE)]
+        self.step_counter = 0
+        self.index = 0
+        self.emodev = 0
+
+    def set_emodev(self, dev):
+        self.emodev = dev
+
+    def update_sensor_values(self, sensors):
+        """Write new sensor value to last_values_list."""
+
+        # c(1,2,4,6,8,9)
+        new_value = sensors['F3']['value'] + sensors['FC5']['value'] + sensors['F7']['value'] + sensors['P7']['value'] + sensors['O2']['value'] + sensors['P8']['value']
+
+        self.last_values_list[self.index] = new_value
+        self.index = (self.index + 1) % WINDOW_SIZE
+        self.step_counter += 1
+
+    def get_brain_activity(self):
+        """Return value between 0 (bad) and 1(good), based on sensor values."""
+
+        if self.step_counter < WINDOW_SIZE:
+            return 0.0
+
+        std = np.std(self.last_values_list, ddof=1) # sample standard deviation
+
+        if std < SD_LOWER_LIMIT:
+            return -1.0
+
+        elif std > SD_UPPER_LIMIT:
+            return 1.0
+
+        else:
+            return 0.0
+
+    def do_start(self):
+
+        with Emotiv(display_output=False, verbose=True) as headset:
+            while self.run:
+
+                packet = headset.dequeue()
+                if packet is None:
+                    continue
+
+                self.update_sensor_values(packet.sensors)
+
+                if self.emodev:
+                    self.emodev.write_message("brain:activity:" + str(self.get_brain_activity()))
+
+                #data = ",".join([
+                #    str(packet.sensors['F3']['value']),
+                #    str(packet.sensors['FC5']['value']),
+                #    str(packet.sensors['AF3']['value']),
+                #    str(packet.sensors['F7']['value']),
+                #    str(packet.sensors['T7']['value']),
+                #    str(packet.sensors['P7']['value']),
+                #    str(packet.sensors['O1']['value']),
+                #    str(packet.sensors['O2']['value']),
+                #    str(packet.sensors['P8']['value']),
+                #    str(packet.sensors['T8']['value']),
+                #    str(packet.sensors['F8']['value']),
+                #    str(packet.sensors['AF4']['value']),
+                #    str(packet.sensors['FC6']['value']),
+                #    str(packet.sensors['F4']['value']),
+                #    str(packet.sensors['X']['value']),
+                #    str(packet.sensors['Y']['value'])
+                #])
+
+                time.sleep(0.0001)
+
+
 
 
 class SocketHandler(websocket.WebSocketHandler):
@@ -13,9 +98,10 @@ class SocketHandler(websocket.WebSocketHandler):
         return True
 
     def open(self):
+        emoDev.set_emodev(self)
         print 'new connection'
-        self.write_message("Hi, client: connection is made ...")
-        tornado.ioloop.IOLoop.instance().add_timeout(datetime.timedelta(seconds=1), self.test)
+        #self.write_message("Hi, client: connection is made ...")
+        #tornado.ioloop.IOLoop.instance().add_timeout(datetime.timedelta(seconds=1), self.test)
 
     def on_message(self, message):
 
@@ -73,12 +159,11 @@ class SocketHandler(websocket.WebSocketHandler):
     def on_close(self):
         print 'connection closed'
 
-    def test(self):
-        self.write_message("GOT SENSOR DATA")
-        tornado.ioloop.IOLoop.instance().add_timeout(datetime.timedelta(seconds=1), self.test)
-
 
 if __name__ == "__main__":
+
+    emoDev = EmoWorker()
+
     app = tornado.web.Application([
         (r'/()', web.StaticFileHandler, {'path': './index.html'}),
         (r'/ws', SocketHandler),
