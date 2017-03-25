11231724f50c677899deb22e6f0d8cc884ff3076
Robert Eisele <robert@xarg.org>
2017-03-25 19:28:41 +0100

    fixed init screen, fixed painter

--- code/app.py
+++ code/app.py
@@ -224,27 +224,27 @@ class EmoWorker:
                 #ROTATE+= (packet.sensors['F8']['value']-115)/4000.0
                 #print str(ROTATE) + " - " + str((packet.sensors['F8']['value']-115)/4000.0)
 
-                data = ",".join([
-                   str(packet.sensors['F3']['value']),
-                   str(packet.sensors['FC5']['value']),
-                   str(packet.sensors['AF3']['value']),
-                   str(packet.sensors['F7']['value']),
-                   str(packet.sensors['T7']['value']),
-                   str(packet.sensors['P7']['value']),
-                   str(packet.sensors['O1']['value']),
-                   str(packet.sensors['O2']['value']),
-                   str(packet.sensors['P8']['value']),
-                   str(packet.sensors['T8']['value']),
-                   str(packet.sensors['F8']['value']),
-                   str(packet.sensors['AF4']['value']),
-                   str(packet.sensors['FC6']['value']),
-                   str(packet.sensors['F4']['value']),
-                   str(packet.sensors['X']['value']),
-                   str(packet.sensors['Y']['value']),
-                   str(dt.now())
-                ])
-
-                print data
+                # data = ",".join([
+                #    str(packet.sensors['F3']['value']),
+                #    str(packet.sensors['FC5']['value']),
+                #    str(packet.sensors['AF3']['value']),
+                #    str(packet.sensors['F7']['value']),
+                #    str(packet.sensors['T7']['value']),
+                #    str(packet.sensors['P7']['value']),
+                #    str(packet.sensors['O1']['value']),
+                #    str(packet.sensors['O2']['value']),
+                #    str(packet.sensors['P8']['value']),
+                #    str(packet.sensors['T8']['value']),
+                #    str(packet.sensors['F8']['value']),
+                #    str(packet.sensors['AF4']['value']),
+                #    str(packet.sensors['FC6']['value']),
+                #    str(packet.sensors['F4']['value']),
+                #    str(packet.sensors['X']['value']),
+                #    str(packet.sensors['Y']['value']),
+                #    str(dt.now())
+                # ])
+                #
+                # print data
 
                 time.sleep(0.0001)
 
